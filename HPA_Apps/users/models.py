56245681eecddc7820dc12ba4948afccf3e557bb
Mohamed Yasser <mohamedyasser5998@gmail.com>
2020-11-03 20:33:43 +0200

    Editing

--- HPA_Apps/users/models.py
+++ HPA_Apps/users/models.py
@@ -10,12 +10,13 @@ from django.dispatch import receiver
 ############# ModelManager
 class CustomUserManager(BaseUserManager):
 
-    ### Custom user model where (((field = email))) is the unique identifier
-    ### for authentication
+    """Custom user model where (((field = email))) is the unique identifier for authentication"""
     def create_user(self,email,password, **extra_fields):
-        ### Create and save User in DB with the given email and password
+        "Create and save User in DB with the given email and password"
+
         if not email:
             raise ValueError(_('The Email must be set'))
+
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
@@ -23,7 +24,7 @@ class CustomUserManager(BaseUserManager):
         return user
 
     def create_superuser(self,email,password, **extra_fields):
-        ### Create and save SuperUser in DB with the given email and password
+        """Create and save SuperUser in DB with the given email and password"""
 
         extra_fields.setdefault('is_superuser',True)
         extra_fields.setdefault('is_active',True)
@@ -31,7 +32,10 @@ class CustomUserManager(BaseUserManager):
 
         if extra_fields.get('is_superuser') is not True:
             raise ValueError(_('Super User must have is_superuser=True.'))
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Super User must have is_staff=True.'))
         return self.create_user(email,password, **extra_fields)
+
 
 ############# User Model
 class CustomUser(AbstractBaseUser,PermissionsMixin):
@@ -39,11 +43,11 @@ class CustomUser(AbstractBaseUser,PermissionsMixin):
     first_name = models.CharField(_('first name'), max_length=30, blank=True)
     last_name = models.CharField(_('last name'), max_length=30, blank=True)
     is_active = models.BooleanField(_('active'),default=True)
-    is_staff=models.BooleanField(_('is stuff'),default=False)
+    is_staff=models.BooleanField(_('is staff'),default=False)
     date_joined = models.DateTimeField(_('date joined'),default=timezone.now)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['first_name','last_name']
 
     objects = CustomUserManager()
 
@@ -54,14 +58,21 @@ class CustomUser(AbstractBaseUser,PermissionsMixin):
     def __str__(self):
         return self.email
 
-        # Return Full name with a space between them
     def get_full_name(self):
+        """Return Full name with a space between them"""
+
         full_name = '%s %s' % (self.first_name, self.last_name)
         return full_name.strip()
 
-        # Send an E-mail to the user
+    def get_short_name(self):
+        """Return short name of user"""
+        return self.first_name
+
+
     def email_user(self, subject, message, from_email=None, **kwargs):
+        """Send an E-mail to the user"""
         send_mail(subject, message, from_email, [self.email], **kwargs)
+
 
 ############# User Profile
 class Profile(models.Model):
