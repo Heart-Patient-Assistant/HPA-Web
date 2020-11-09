307f5028a9f388034d476b28ca67a220a44e71b6
Mohamed Yasser <mohamedyasser5998@gmail.com>
2020-11-10 01:04:08 +0200

    blog model

--- HPA_Apps/blogs/models.py
+++ HPA_Apps/blogs/models.py
@@ -1,7 +1,44 @@
 from django.db import models
 
 # Create your models here.
+
 class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft','Draft'),
+        ('published','Published'),
+    )
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200,unique_for_date='publish')
     author = models.ForeignKey("users.CustomUser",on_delete=models.CASCADE)
     content = models.TextField(blank=True,null=True)
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    upadted = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
     image = models.FileField(upload_to='images/',blank=True,null=True)
+    tags = TaggableManager()
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',args=[self.publish.year,self.publish.month,self.publish.day,self.slug])
+
+## Name should be edited to take the name of the person
+class Comment(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
