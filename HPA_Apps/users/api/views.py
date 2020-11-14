ef8c8c7f9e8cd83c660b32a20611d67323958daf
Mahdi <a.b.mahdey@gmail.com>
2020-11-14 16:36:58 +0200

    1-creating function for registration   and specifying the permission to anyone   and consumer can access through get to get the requirements to post

--- HPA_Apps/users/api/views.py
+++ HPA_Apps/users/api/views.py
@@ -1,1 +1,27 @@
 from rest_framework.response import Response
+from rest_framework.decorators import api_view,permission_classes
+from .serializers import CreateAccountSerializer
+from rest_framework.authtoken.models import Token
+from rest_framework import permissions
+
+
+@api_view(['POST','GET'])
+@permission_classes([permissions.AllowAny])
+def createaccount(request):
+    serializer=CreateAccountSerializer(data=request.data)
+    
+    data={}
+    
+    if serializer.is_valid():
+        account=serializer.save()
+        
+        data['created']='account successfully created'
+        data['email']=account.email
+
+        token=Token.objects.get(user=account).key
+        data['token']=token
+    
+    else:
+        data=serializer.errors  #returning erros in serializer class
+    
+    return Response(data)
