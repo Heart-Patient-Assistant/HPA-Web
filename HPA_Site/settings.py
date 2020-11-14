e01d12d45560f4752cb376d7371be86994c37f6c
Mahdi <a.b.mahdey@gmail.com>
2020-11-14 16:21:27 +0200

    modyfing setting authentication token

--- HPA_Site/settings.py
+++ HPA_Site/settings.py
@@ -57,10 +57,10 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+    'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ]
 }
