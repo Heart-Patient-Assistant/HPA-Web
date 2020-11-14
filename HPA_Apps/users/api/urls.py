df215d999e95c017ff406f2793b247eed3ee5d40
Mahdi <a.b.mahdey@gmail.com>
2020-11-14 16:23:35 +0200

    -specifying registration endpoint -specifying endpoint for creating new token or retrieve current token for user using his credentials

--- HPA_Apps/users/api/urls.py
+++ HPA_Apps/users/api/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
+
+
+#getting token for authenticated user[tutorial]
+#https://www.django-rest-framework.org/api-guide/authentication/#apache-mod_wsgi-specific-configuration
+from rest_framework.authtoken.views import obtain_auth_token
+
 from . import views
+
+
 app_name = "users_api"
 
 urlpatterns = [
-
+    path('signup/',views.createaccount,name='createaccount'),
+    path('gettoken/',obtain_auth_token,name='gettoken'),
 ]
