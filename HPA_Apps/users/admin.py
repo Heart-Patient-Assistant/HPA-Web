0b6cb6968dbaadd48ee1f2f681122642b0c3163c
Mohamed Yasser <mohamedyasser5998@gmail.com>
2020-11-15 17:59:48 +0200

    editing users

--- HPA_Apps/users/admin.py
+++ HPA_Apps/users/admin.py
@@ -1,28 +1,34 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import CustomUser
+from .models import CustomUser,Patient,Doctor
 from .forms import CustomUserCreationForm,CustomUserChangeForm
 
 # Register your models here.
-
+@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
-    model = CustomUser
     list_display = ('email','is_active',)
     list_filter = ('email', 'is_active',)
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
+        (None, {'fields': ('email', 'password','first_name','last_name','type')}),
         ('Permissions', {'fields': ('is_staff', 'is_active')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active','type','first_name','last_name')}
         ),
     )
     search_fields = ('email',)
     ordering = ('email',)
 
-admin.site.register(CustomUser, CustomUserAdmin)
+
+@admin.register(Patient)
+class PatientAdmin(CustomUserAdmin):
+    readonly_fields = ('type',)
+
+@admin.register(Doctor)
+class DoctorAdmin(CustomUserAdmin):
+    readonly_fields = ('type',)
--- HPA_Apps/users/models.py
+++ HPA_Apps/users/models.py
@@ -69,6 +69,12 @@ class CustomUser(AbstractBaseUser,PermissionsMixin):
     def __str__(self):
         return self.email
 
+    # def has_perm(self, perm, obj=None):
+    #     return self.is_admin
+    #
+    # def has_module_perms(self,app_label):
+    #     return True
+
     def get_full_name(self):
         """Return Full name with a space between them"""
 
@@ -80,9 +86,9 @@ class CustomUser(AbstractBaseUser,PermissionsMixin):
         return self.first_name
 
 
-    def email_user(self, subject, message, from_email=None, **kwargs):
-        """Send an E-mail to the user"""
-        send_mail(subject, message, from_email, [self.email], **kwargs)
+    # def email_user(self, subject, message, from_email=None, **kwargs):
+    #     """Send an E-mail to the user"""
+    #     send_mail(subject, message, from_email, [self.email], **kwargs)
 
 @receiver(post_save,sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
