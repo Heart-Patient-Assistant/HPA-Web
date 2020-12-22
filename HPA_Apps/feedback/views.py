29c0fe7351ab37072532f7039052a1c715e99e61
MHDI3 <a.b.mahdey@gmail.com>
2020-12-22 16:17:39 +0200

    creating givefeedback function

--- HPA_Apps/feedback/views.py
+++ HPA_Apps/feedback/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from . import serializers
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.decorators import api_view,permission_classes
 
 # Create your views here.
+@api_view(["POST","GET"])
+@permission_classes([permissions.AllowAny])
+def giveFeedback(request):
+    data={}
+    serializer=serializers.FeedbackSerializer(data=request.data)
+   
+    if request.method=="POST":
+        if serializer.is_valid():
+            serializer.save()
+            data["sucess"]="successfully feeded"
+        else:
+            data['fail']="fail to feed"
+        
+    else:
+        data["rate"]="assign your rate"
+        data["feedback_category"]="assign the category"
+        data["feedback_message"]="assign the message"
+         
+        
+    return Response(data)
+    
