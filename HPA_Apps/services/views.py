cb895f323e0d5d3f1c5f7ef934499caec68348be
MHDI3 <a.b.mahdey@gmail.com>
2021-04-15 16:10:55 +0200

    getting predication api,vanila function

--- HPA_Apps/services/views.py
+++ HPA_Apps/services/views.py
@@ -3,6 +3,15 @@ from . import serializers
 from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework.decorators import api_view,permission_classes
+from HPA_Apps.users.forms import MedicalRecordForm
+
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+
+
+
  
 import pickle
 import os 
@@ -11,7 +20,7 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 # Create your views here.
 @api_view(['POST','GET'])
 @permission_classes([permissions.AllowAny])
-def getPrediction(request):     
+def getPredictionApi(request):     
     #unpacking
     serializer=serializers.MLpredictionserializer(data=request.data)
     response={}
@@ -30,11 +39,38 @@ def getPrediction(request):
         prediction=logmodel.predict([array])
         
         response['prediction']= "don't worry" if prediction==0 else "we got your back, we reccommed to contact one of our doctors "
-        
-        response['worry']=f'{(logmodel.predict_proba([array])[0][1])*100:.1f}%'      
-    
+        response['worry']=f'{(logmodel.predict_proba([array])[0][1])*100:.2f}%'          
     
     else:
         response['respose']=serializer.errors
         
     return Response(response)
+
+######################################################
+@permission_classes([permissions.AllowAny])
+def getPrediction(request):
+    if request.method=='POST':
+        form=MedicalRecordForm(request.POST)
+        if form.is_valid():
+            array=(list(form.cleaned_data.values()))
+        
+            filepath=os.path.join(dir_path,"model.sav")
+            with open(filepath, 'rb') as file:  
+                logmodel= pickle.load(file)
+            
+            #predicting 
+            prediction=logmodel.predict([array])
+
+            response={}
+            response['prediction']= "don't worry" if prediction==0 else "we got your back, we reccommed to contact one of our doctors "
+            
+            response['worry']=f'{(logmodel.predict_proba([array])[0][1])*100:.2f}%'
+            
+            
+            return HttpResponseRedirect(
+                reverse('services:getresult',
+                kwargs={'result':f'{response["prediction"]} -- {response["worry"] }'}))
+
+    return render(request,'post_form.html',{'form':MedicalRecordForm()})
+  
+    #
