0bc6ab8391e17ccb5ef7baa69e43292058231367
Mohamed Yasser <mohamedyasser5998@gmail.com>
2021-04-24 23:32:16 +0200

    blog posts and detail

--- HPA_Apps/blogs/admin.py
+++ HPA_Apps/blogs/admin.py
@@ -1,52 +1,9 @@
-from django import forms
+from django.contrib import admin
 from .models import Post, Category, Comment
 
-# to hard code choices
-# choices = [('sport','sport'),('cars','cars'),('Big Choice','Big Choice')]
-choices = Category.objects.all().values_list("name", "name")
-choices_list = []
-for item in choices:
-    choices_list.append(item)
+# Register your models here.
 
-
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ("title", "title_tag", "author", "category", "body", "header_image")
-
-        widgets = {
-            "title": forms.TextInput(attrs={"class": "form-control"}),
-            "title_tag": forms.TextInput(attrs={"class": "form-control"}),
-            "author": forms.Select(attrs={"class": "form-control"}),
-            "category": forms.Select(
-                choices=choices_list, attrs={"class": "form-control"}
-            ),
-            "body": forms.Textarea(
-                attrs={"class": "form-control", "rows": 4, "cols": 15}
-            ),
-        }
-
-
-class EditForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ("title", "author", "body")
-
-        widgets = {
-            "title": forms.TextInput(attrs={"class": "form-control"}),
-            "author": forms.Select(attrs={"class": "form-control"}),
-            "body": forms.Textarea(attrs={"class": "form-control"}),
-        }
-
-
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        fields = ("name", "body")
-
-        widgets = {
-            "name": forms.TextInput(attrs={"class": "form-control"}),
-            "body": forms.Textarea(
-                attrs={"class": "form-control", "rows": 2, "cols": 10}
-            ),
-        }
+admin.site.register(Post)
+admin.site.register(Category)
+# admin.site.register(Profile)
+admin.site.register(Comment)
