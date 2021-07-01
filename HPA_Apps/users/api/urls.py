60879efcd2860666b53658ee4411ec4adab75f4b
MHDI3 <a.b.mahdey@gmail.com>
2021-07-01 14:51:32 +0200

    modifing obtain token request function

--- HPA_Apps/users/api/urls.py
+++ HPA_Apps/users/api/urls.py
@@ -4,6 +4,8 @@ from django.urls import path
 # getting token for authenticated user[tutorial]
 # https://www.django-rest-framework.org/api-guide/authentication/#apache-mod_wsgi-specific-configuration
 from rest_framework.authtoken.views import obtain_auth_token
+from .views import CustomAuthToken
+
 
 from . import views
 
@@ -12,6 +14,6 @@ app_name = "users_api"
 
 urlpatterns = [
     path("signup/", views.createaccount, name="createaccount"),
-    path("signin/", obtain_auth_token, name="signin"),
+    path("signin/", CustomAuthToken.as_view(), name="signin"),
     path("editprofile/", views.editprofile, name="edit"),
 ]
