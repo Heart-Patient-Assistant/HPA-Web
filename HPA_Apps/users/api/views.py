bd57f1b716bde4464bfae5d04dad5a31507c269a
MHDI3 <a.b.mahdey@gmail.com>
2021-07-01 14:50:41 +0200

    enhance the signin api response with user type

--- HPA_Apps/users/api/views.py
+++ HPA_Apps/users/api/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view, permission_classes
 from .serializers import CreateAccountSerializer, EditingProfileSerializer
 from rest_framework.authtoken.models import Token
 from rest_framework import permissions, serializers
-
+from rest_framework.authtoken.views import ObtainAuthToken
 
 from HPA_Apps.users.models import Profile
 
@@ -74,3 +74,18 @@ def editprofile(request):
         data["birth_date"] = "input your birth data y-m-d "
 
     return Response(data)
+
+
+class CustomAuthToken(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'type': user.type,
+            'email': user.email
+        })
