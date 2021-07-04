9d1630c3ccbaded9cfd12d3f3fc2db28481be5a1
MHDI3 <a.b.mahdey@gmail.com>
2021-07-04 17:57:12 +0200

    setting anchor to egc prediction

--- HPA_Apps/services/urls.py
+++ HPA_Apps/services/urls.py
@@ -6,5 +6,7 @@ urlpatterns =[
 path('', TemplateView.as_view(template_name="servicesList.html"),name="servicesList"),
 path('getpredictionapi/',views.getPredictionApi,name='getpredictionApi'),
 path('getprediction/',views.getPrediction,name="getprediction"),
+path('egcapi/',views.egcApi,name='egcApi'),
+path('egc/',views.egc,name="egc"),
 path('<str:result>', TemplateView.as_view(template_name="get_result.html"),name='getresult'),
 ]
