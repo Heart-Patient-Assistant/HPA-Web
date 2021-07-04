7983c98109b8c1b916f6322867efcbad0cc8d5a1
MHDI3 <a.b.mahdey@gmail.com>
2021-07-04 17:58:25 +0200

    creating egc function view api and vanila

--- HPA_Apps/services/views.py
+++ HPA_Apps/services/views.py
@@ -4,10 +4,20 @@ from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework.decorators import api_view,permission_classes
 from HPA_Apps.users.forms import MedicalRecordForm
+from .forms import Egc
 
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+
+
+
+#cnn
+import numpy as np
+import pandas as pd
+from keras.models import load_model
+import wfdb
+from numpy import savetxt
 
 
 
@@ -74,3 +84,108 @@ def getPrediction(request):
     return render(request,'prediction_form.html',{'form':MedicalRecordForm()})
   
     #
+
+@permission_classes([permissions.AllowAny])
+def egc(request):
+    response={}
+    if request.method=='POST':
+        form=Egc(request.POST,request.FILES)
+       
+        if form.is_valid():
+           
+            dirpath=os.path.join(dir_path,"cnn")
+
+            '''
+            segHeader=os.path.join(dirpath,"seg.hea")
+            with open(segHeader, 'wb+') as destination:
+                  for chunk in request.FILES['egcheader'].chunks():
+                    destination.write(chunk)
+            
+            segData=os.path.join(dirpath,"seg.data")
+            with open(segData, 'wb+') as destination:
+                  for chunk in request.FILES['egcheader'].chunks():
+                    destination.write(chunk)
+           '''
+  # set the the file as a record 
+
+            record = wfdb.rdrecord(dirpath+'/seg04',sampfrom=0,sampto=185 ,channels=[2])
+            data = record.p_signal
+
+
+            
+            savetxt(dirpath+'/data4.csv',data, delimiter=',', newline=",")
+
+            datapath=os.path.join(dirpath,"data4.csv")
+            test = pd.read_csv(datapath, header = None)
+
+
+            
+            model=load_model(dirpath+"/model.h5")
+    
+
+
+            y_pred=model.predict(test)
+            categories=["Normal beat", "unknown Beat", "Ventricular ectopic beat", "Supraventricular ectopic beat", "Fusion Beat"]
+            categoryIndex=np.argmax(y_pred)
+            prediction_cat=categories[categoryIndex]
+            print(prediction_cat)
+            response["prediction"]=prediction_cat
+
+            return HttpResponseRedirect(
+                reverse('services:getresult',
+                kwargs={'result':f'{response["prediction"]}'}))
+
+    return render(request,'prediction_form.html',{'form':Egc()})
+  
+
+@api_view(['POST','GET'])
+@permission_classes([permissions.AllowAny])
+def egcApi(request): 
+        serializer=serializers.EgcSerializer(data=request.data)
+    
+        response={}    
+        if serializer.is_valid():
+                #getiing values out of bject 
+            dirpath=os.path.join(dir_path,"cnn")
+
+            '''
+            segHeader=os.path.join(dirpath,"seg.hea")
+            with open(segHeader, 'wb+') as destination:
+                  for chunk in request.FILES['egcheader'].chunks():
+                    destination.write(chunk)
+            
+            segData=os.path.join(dirpath,"seg.data")
+            with open(segData, 'wb+') as destination:
+                  for chunk in request.FILES['egcheader'].chunks():
+                    destination.write(chunk)
+           '''
+  # set the the file as a record 
+
+            record = wfdb.rdrecord(dirpath+'/seg04',sampfrom=0,sampto=185 ,channels=[2])
+            data = record.p_signal
+
+
+            
+            savetxt(dirpath+'/data4.csv',data, delimiter=',', newline=",")
+
+            datapath=os.path.join(dirpath,"data4.csv")
+            test = pd.read_csv(datapath, header = None)
+
+
+            
+            model=load_model(dirpath+"/model.h5")
+    
+
+
+            y_pred=model.predict(test)
+            categories=["Normal beat", "unknown Beat", "Ventricular ectopic beat", "Supraventricular ectopic beat", "Fusion Beat"]
+            categoryIndex=np.argmax(y_pred)
+            prediction_cat=categories[categoryIndex]
+            print(prediction_cat)
+
+            response['prediction']= prediction_cat
+        
+        else:
+            response['respose']=serializer.errors
+            
+        return Response(response)
